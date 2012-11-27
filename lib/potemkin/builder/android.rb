e47946064fb539e8eea94e4d8509215fb234f28e
Bob Van Landuyt <bob.vanlanduyt@gmail.com>
2012-11-27 23:29:38 +0100

    Add some documentation

--- lib/potemkin/builder/android.rb
+++ lib/potemkin/builder/android.rb
@@ -1,6 +1,7 @@
 module Potemkin
   class AndroidBuilder < Builder
 
+    # android needs the ANDROID_HOME dir to be set in the environment
     def env_vars
       { "ANDROID_HOME" => config.sdk_root }
     end
@@ -11,10 +12,12 @@ module Potemkin
       "ant -f #{android_project_dir}/build.xml #{config.build_type}"
     end
 
+    # this will clean out the bin directories within a project
     def clean_command
       "ant -f #{android_project_dir}/build.xml clean"
     end
 
+    # The subfolder where the actual app is located
     def android_project_dir
       raise "Please add project dir" unless config.android_project_dir
       config.android_project_dir
