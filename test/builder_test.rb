25883a83452482e9771517fe5d19cec5ce87e220
Bob Van Landuyt <bob.vanlanduyt@gmail.com>
2012-11-27 23:58:27 +0100

    Correct descriptions

--- test/builder_test.rb
+++ test/builder_test.rb
@@ -3,20 +3,29 @@ describe Potemkin::Builder do
 
   before do
     @builder = Potemkin::Builder.new
+    @builder.stubs(:clean_command).returns("")
+    @builder.stubs(:build_command).returns("")
     @builder.stubs(:config).returns(Potemkin::Configuration.new(:sdk_root => "/some/path", :android_project_dir => "dir", :build_type => "debug"))
   end
 
   it "should be able to wrap a block in env vars" do
-
-  end
-
-  it "should run the build command with the android home set" do
     android_home = "UNKNOWN"
     @builder.with_env_vars("ANDROID_HOME" => "~/her-o") do
       android_home = ENV["ANDROID_HOME"]
     end
     assert_equal "~/her-o", android_home
   end
+
+  it "should run clean commands with environment variables" do
+    @builder.expects(:with_env_vars)
+    @builder.clean
+  end
+
+  it "should run clean commands with environment variables" do
+    @builder.expects(:with_env_vars)
+    @builder.build
+  end
+
 
 
 end
