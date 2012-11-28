0f7da48244acacdeb1bee24dc5a142be022548d9
Bob Van Landuyt <bob.vanlanduyt@gmail.com>
2012-11-28 23:14:36 +0100

    Add the environment test

--- test/builder_android_test.rb
+++ test/builder_android_test.rb
@@ -7,8 +7,10 @@ describe Potemkin::AndroidBuilder do
   end
 
   it "should run the build command with the android home set" do
-    @builder.build
-    # TODO: Prove it!
+    proc = Proc.new { ENV["ANDROID_HOME"] }
+    @builder.stubs(:build).returns(proc.call)
+    home = @builder.build
+    assert_equal "/some/path", home
   end
 
   it "should run a build command" do
@@ -21,6 +23,14 @@ describe Potemkin::AndroidBuilder do
     command = "ant -f dir/build.xml clean"
     Potemkin.expects(:run).with(command).once
     @builder.clean
+  end
+
+  it "should fail if the project dir is not set" do
+    @builder_without_dir = Potemkin::AndroidBuilder.new
+    @builder_without_dir.stubs(:config).returns(Potemkin::Configuration.new(:sdk_root => "/some/path", :build_type => "debug"))
+    builder_error = @builder_without_dir.build rescue "RESCUED"
+    assert_equal "RESCUED", builder_error
+
   end
 
 
