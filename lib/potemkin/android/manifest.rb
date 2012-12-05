7cd200ac0d41fd27051684ddf4ca211a901409a5
Bob Van Landuyt <bob.vanlanduyt@gmail.com>
2012-12-05 23:05:07 +0100

    Implement versioning for android

--- lib/potemkin/android/manifest.rb
+++ lib/potemkin/android/manifest.rb
@@ -3,16 +3,34 @@ module Potemkin
   module Android
     class Manifest
 
-      def initialize(manifest_file)
-        @xml_file = Nokogiri::XML(File.open(manifest_file))
+      def initialize(manifest_path)
+        @manifest_path = manifest_path
+      end
+
+      def manifest_file
+        @manifest_file ||= Nokogiri::XML(File.open(@manifest_path))
       end
 
       def version_code
-        @xml_file.css("manifest").first["versionCode"].to_i
+        manifest_file.css("manifest").first["versionCode"].to_i
+      end
+
+      def version_code=(value)
+        manifest_file.css("manifest").first["android:versionCode"] = value.to_s
       end
 
       def version_name
-        @xml_file.css("manifest").first["versionName"]
+        manifest_file.css("manifest").first["versionName"]
+      end
+
+      def version_name=(value)
+        manifest_file.css("manifest").first["android:versionName"] = value.to_s
+      end
+
+      def save!
+        file = File.open(@manifest_path, "w")
+        file.write(manifest_file)
+        file.close
       end
 
       def self.discovered
--- lib/potemkin/android/version.rb
+++ lib/potemkin/android/version.rb
@@ -1,6 +1,32 @@
 module Potemkin
   module Android
-    class Version
+    class Version < Potemkin::Version
+
+      def initialize(manifest_file = nil)
+        if manifest_file
+          @manifest = Potemkin::Android::Manifest.new(manifest_file)
+        else
+          @manifest = Potemkin::Android::Manifest.discovered
+        end
+        @version_code = @manifest.version_code
+        super(@manifest.version_name)
+      end
+
+      def bump(type)
+        if %w(major minor patch).include?(type.downcase)
+          @version_code +=1
+          send("bump_#{type}")
+          write!
+        else
+          raise "Don't now how to bump the #{type}amathing"
+        end
+      end
+
+      def write!
+        @manifest.version_code = @version_code
+        @manifest.version_name = self
+        @manifest.save!
+      end
 
     end
   end
