760063473911685b71dd9ca083ada217565af09b
Bob Van Landuyt <bob.vanlanduyt@gmail.com>
2012-12-24 16:33:36 +0100

    Implement git stuff

--- lib/potemkin/git.rb
+++ lib/potemkin/git.rb
@@ -2,21 +2,48 @@ require "rugged"
 module Potemkin
   class Git
 
-    def self.repository
-      @repository ||= Rugged::Repository.new(".")
+    attr_accessor :repository_path
+    def initialize(path = ".")
+      @repository_path = path
     end
 
-    def self.last_commit_is_a_tag?
-
+    def repository
+      @repository ||= Rugged::Repository.new(repository_path)
     end
 
-    def self.current_branch
-      repository.branches.detect {|b| b.target == repository.head.target}.name
+    def current_sha
+      repository.head.target
     end
 
-    def self.changelog
-      tag = `git describe --abbrev=0 --tags`.strip
-      `git cat-file tag #{tag} | sed -e '1,/^$/d'`
+    def current_tag
+      tag = tag_for_commit(current_sha)
+      tag ? tag.name : nil
+    end
+
+    def current_branch
+      branch = repository.branches.detect {|b| b.target == repository.head.target}
+      branch ? branch.name : nil
+    end
+
+    def tag_for_commit(sha)
+      tag_ref = repository.refs(/tags/).detect do |ref|
+        repository.lookup(ref.target).target.oid == sha
+      end
+      repository.lookup(tag_ref.target) if tag_ref
+    end
+
+    def changelog
+      commit = repository.lookup(current_sha).parents.first
+      messages = []
+      while(commit && tag_for_commit(commit.oid).nil?) do
+        messages << commit.message.strip
+        commit = commit.parents.first
+      end
+      messages
+    end
+
+    def config
+      Potemkin.config
     end
 
   end
